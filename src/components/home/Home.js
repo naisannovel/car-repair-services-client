9e5a7b6061ffc9d752a15667bfa4f24fe2225ad2
naisannovel <naisannovel@gmail.com>
2022-01-22 23:53:53 +0600

    update

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,18 +2,27 @@ import React, { lazy, Suspense } from "react";
 import NavBar from "./NavBar";
 import Spinner from '../utilities/Spinner';
 
-const Hero = lazy(()=> import("./Hero"));
-const About = lazy(()=> import("./about/About"));
-const HowWeWorks = lazy(()=> import("./HowWeWorks"));
-const OurServices = lazy(()=> import("./OurServices"));
-const VehicleBrand = lazy(()=> import("./VehicleBrand"));
-const Reviews = lazy(()=> import("./Reviews"));
-const Contact = lazy(()=> import("./contact/Contact"));
-const Footer = lazy(()=> import("./Footer"));
+// const Hero = lazy(()=> import("./Hero"));
+// const About = lazy(()=> import("./about/About"));
+// const HowWeWorks = lazy(()=> import("./HowWeWorks"));
+// const OurServices = lazy(()=> import("./OurServices"));
+// const VehicleBrand = lazy(()=> import("./VehicleBrand"));
+// const Reviews = lazy(()=> import("./Reviews"));
+// const Contact = lazy(()=> import("./contact/Contact"));
+// const Footer = lazy(()=> import("./Footer"));
+
+import Hero from './Hero';
+import About from './about/About';
+import HowWeWorks from './HowWeWorks.jsx';
+import OurServices from './OurServices.jsx';
+import VehicleBrand from './VehicleBrand.jsx';
+import Reviews from './Reviews.jsx';
+import Contact from './contact/Contact';
+import Footer from './Footer';
 
 const Home = () => {
   return (
-    <Suspense fallback={<Spinner/>}>
+    <>
       <NavBar/>
       <Hero/>
       <About />
@@ -23,8 +32,27 @@ const Home = () => {
       <Reviews/>
       <Contact/>
       <Footer/>
-    </Suspense>
+    </>
   );
 };
 
 export default Home;
+
+
+// const Home = () => {
+//   return (
+//     <Suspense fallback={<Spinner/>}>
+//       <NavBar/>
+//       <Hero/>
+//       <About />
+//       <HowWeWorks/>
+//       <OurServices/>
+//       <VehicleBrand/>
+//       <Reviews/>
+//       <Contact/>
+//       <Footer/>
+//     </Suspense>
+//   );
+// };
+
+// export default Home;
